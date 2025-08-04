b1517ba4d04211f5229016834fcf967c22542f4b
Ana Letícia <152559893+analeticiarc@users.noreply.github.com>
2025-08-04 15:25:48 -0300

    toast container

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,18 @@ function App() {
   return (
     <BrowserRouter>
       <AppRoutes />
-      <ToastContainer />
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
     </BrowserRouter>
   );
 }
