e341ef043039c0d9e689c6fa39be96e804dc1715
Ana Letícia <152559893+analeticiarc@users.noreply.github.com>
2025-08-04 15:34:38 -0300

    add rotas

--- src/services/packageService.js
+++ src/services/packageService.js
@@ -5,39 +5,39 @@ import api from './api';
 const packageService = {
     
     getPackages: async () => {
-        const response = await api.get('/TravelPackage/GetAllPackages');
+        const response = await api.get('/TravelPackageControler/GetAllPackages');
         return response.data;
     },
 
     getPackageById: async (id) => {
-        const response = await api.get(`/TravelPackage/GetById/${id}`);
+        const response = await api.get(`/TravelPackageControler/GetById/${id}`);
         return response.data;
     } ,
 
     createPackage: async (travelPackage) =>{
-        const response = await api.post('/TravelPackage/Create', travelPackage);
+        const response = await api.post('/TravelPackageControler/Create', travelPackage);
         return response.data;
     },
 
     updatePackage: async (id, travelPackage) =>{
-        const response = await api.put(`/TravelPackage/Update/${id}`, travelPackage);
+        const response = await api.put(`/TravelPackageControler/Update/${id}`, travelPackage);
         return response.data;
     },
     
     deletePackage: async (id) => {
-        const response = await api.delete(`/TravelPackage/Delete/${id}`);
+        const response = await api.delete(`/TravelPackageControler/Delete/${id}`);
         return response.data;
     },
     getByDepartureDate: async (initialDate, finalDate) => {
-        const response = await api.get(`/TravelPackage/GetByDepartureDate/${initialDate, finalDate}`);
+        const response = await api.get(`/TravelPackageControler/GetByDepartureDate/${initialDate, finalDate}`);
         return response.data;
     },
     getByDestination: async (destination) => {
-        const response = await api.get(`/TravelPackage/GetByDestination/${destination}`);
+        const response = await api.get(`/TravelPackageControler/GetByDestination/${destination}`);
         return response.data;
     },
     getByValue: async (minValue, maxValue) => {
-        const response = await api.get(`/TravelPackage/GetByValue/${minValue, maxValue}`);
+        const response = await api.get(`/TravelPackageControler/GetByValue/${minValue, maxValue}`);
         return response.data;
     },
 
